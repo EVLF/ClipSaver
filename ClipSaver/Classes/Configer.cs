3867a664b78ab9da797ebbd04940984decfa9017
EVLF <54191699+EVLF@users.noreply.github.com>
2022-02-06 20:19:46 +0200

    Add files via upload

---
+++ ClipSaver/Classes/Configer.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Vip.Notification;
+
+namespace ClipSaver
+{
+    class Configer
+    {
+       public  enum Targets{
+            tostartup= 1,
+            lastclip= 2
+        }
+
+
+
+        public static void WriteToxml(Targets Target,string val)
+        {
+            string target = Gettarget(Target);
+            if (target == "null")
+            {
+                return;
+            }
+            XElement t = XElement.Load(statics.ConfigPath);
+            t.Element("setting").Element(target).Value = val;
+            t.Save(statics.ConfigPath);
+           
+        }
+
+
+        public static string Readxml(Targets Target)
+        {
+            string target = Gettarget(Target);
+            if (target == "null")
+            {
+                return "null";
+            }
+            XElement t = XElement.Load(statics.ConfigPath);
+            return t.Element("setting").Element(target).Value;
+
+        }
+
+
+        public static void SaveData( string data)
+        {
+
+            XElement t = XElement.Load(statics.datapath);
+            var a = t.Descendants("setting").FirstOrDefault();
+            a.Add(new XElement("data", data));
+            t.Save(statics.datapath);
+            
+
+        }
+
+        public static string[] LoadData()
+        {
+
+            XElement t = XElement.Load(statics.datapath);
+            int count = t.Element("setting").Elements("data").Count();
+
+
+            string[] holdarry = new string[count];
+            int counter = 0;
+
+            foreach (var status in t.Elements("setting").Elements())
+            {
+                string tweet = status.Value;
+                if (status.Value.Length > 0)
+                {
+                    holdarry[counter] = tweet;
+                    counter += 1;
+                }
+                else
+                {
+                    holdarry[counter] = null;
+                    counter += 1;
+                }
+                
+            }
+
+
+            return holdarry;
+           
+
+        }
+
+        private static string Gettarget(Targets target)
+        {
+            switch (target)
+            {
+                case Targets.tostartup:
+                    return "tostartup";
+
+                case Targets.lastclip:
+                    return "lastclip";
+            }
+            return "null";
+        }
+
+        public static void Hanelstartup(bool addtostartup) {
+
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                if (addtostartup)
+                {
+
+                    rk.SetValue("ClipSaver","\""+ Application.ExecutablePath+"\""+ " /fromstart");
+                }
+                else
+                {
+                    try
+                    {
+
+                        rk.DeleteValue("ClipSaver");
+                    }
+                    catch (Exception b) { }
+                }
+
+            }
+
+           
+        }
+
+        public static void showalert(string msg)
+        {
+            Alert.ShowCustom(msg, false, Properties.Resources.mylogo, Color.Black);
+        }
+
+    }
+}
